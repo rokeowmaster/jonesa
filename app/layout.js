68eb515f7bca6da25b482d0605bc5a7d238eb84d
rokeowmaster <rokeowmaster@gmail.com>
2025-03-27 18:13:39 +0300

    clark

--- app/layout.js
+++ app/layout.js
@@ -20,8 +20,7 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <ClerkProvider publicKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY}>
-
+    <ClerkProvider publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY}>
     <html lang="en">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
@@ -29,6 +28,6 @@ export default function RootLayout({ children }) {
        <CartProvider>{children}</CartProvider> 
       </body>
     </html>
-    // </ClerkProvider>
+    </ClerkProvider>
   );
 }
